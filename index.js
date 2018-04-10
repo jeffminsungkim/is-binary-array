b344f93500e492b4ad16ed3a7414acfab0832a10
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-04-10 13:37:02 +0900

    improve performance

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = input => {
 	if (Object.prototype.toString.call(input) !== '[object Array]') {
 		throw new TypeError(`Expected an array, got ${typeof input}`);
 	}
-	for (let i = input.length - 1; i >= 0; --i) {
+	for (let i = 0; i < input.length; i++) {
 		if (input[i] !== 0 && input[i] !== 1) {
 			return false;
 		}
